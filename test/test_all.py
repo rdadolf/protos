1a9705481707619185a56f8761962f05d2ef25c8
Bob Adolf <rdadolf@seas.harvard.edu>
2014-11-17 18:33:07 -0500

    Added a driver for running many regression tests.
    It's dumb, but it will work for now.

---
+++ test/test_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import subprocess as sub
+import os
+from functools import reduce
+import signal
+
+failed = 0
+attempted = 0
+for test in os.listdir('experiments'):
+  cmd = 'protos '+os.path.join('experiments',test)
+  proc = sub.Popen(cmd, shell=True, universal_newlines=True, stdout=sub.PIPE, stderr=sub.PIPE)
+  attempted += 1
+  try:
+    (out,err) = proc.communicate(timeout=10) # 10 seconds
+    # FIXME? this output is not recorded. For now, if something fails, just run
+    #        it manually and investigate.
+    ret = proc.returncode
+  except sub.TimeoutExpired:
+    proc.kill()
+    ret = -1 * signal.SIGKILL
+  s='{experiment:.<50}[{status}]'
+  if ret!=0:
+    print(s.format(experiment=test,status='FAIL'))
+    failed += 1
+  else:
+    print(s.format(experiment=test,status='PASS'))
+
+print(80*'_')
+print('{passed}/{attempted} tests passed.'.format(passed=attempted-failed, attempted=attempted))
