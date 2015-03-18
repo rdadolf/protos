67a8c5feed03a6af00ad7680da65fa672798e307
Bob Adolf <rdadolf@seas.harvard.edu>
2015-03-18 10:31:02 -0400

    Cleaned up modules, minor fix to protos.
    Reorganized the protos __init__.
     - functions are exported near where they are imported (more sane)
     - the protos namespace is less cluttered with bad module names
    Fixed an embarassing shadowing bug in protos (a=func=b vs. a=func(b),
    where func was something important).
    Removed the apparently unused utility.py file.

--- protos/__init__.py
+++ protos/__init__.py
@@ -1,16 +1,24 @@
 # Select the features we need
+__all__ = []
 from .protocol_support import protocol, home, call
+__all__.extend(['protocol','home','call'])
 from .experiment_support import experiment
+__all__.extend(['experiment'])
 from .data_bundles import Data_Bundle as Bundle, Void_Bundle
+__all__.extend(['Bundle','Void_Bundle'])
 from .config import config
+__all__.extend(['config'])
 from .log import log
-# Clean up namespace
-del protocol_support, experiment_support
-# Export those features
-__all__=[\
-  'protocol', 'home', 'call',\
-  'experiment',\
-  'Bundle','Void_Bundle',\
-  'config',\
-  'log'
-]
+__all__.extend(['log'])
+
+# Enable submodules
+__all__.extend(['query'])
+
+# Clean up all files in this directory which are not shadowed by a call
+# config (shadowed)
+del data_bundles
+del experiment_support
+del fs_layout
+# log (shadowed)
+del protocol_support
+del storage
