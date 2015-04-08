3e615d3657d452e8c851ce33f40fce9c728598fb
Bob Adolf <rdadolf@gmail.com>
2015-04-08 18:01:37 -0400

    Fixed exact_experiment() and exact_bundle().
    Works as advertised now.

--- protos/query.py
+++ protos/query.py
@@ -10,6 +10,10 @@ class Query_Result:
     self._datastore = datastore
     self._xid = xid
 
+  @property
+  def id(self):
+    return str(self._xid)
+
   def metadata(self):
     return self._datastore.read_experiment_metadata(self._xid)
 
@@ -17,9 +21,35 @@ class Query_Result:
     bundles = self._datastore.find_bundles(pattern, self._xid)
     return bundles
 
+  def exact_bundle(self,bid):
+    bundles = self._datastore.find_bundles({'metadata':{'id':str(bid)}}, self._xid)
+    if len(bundles)==0:
+      logging.debug('No bundle id matched "'+str(bid)+'"')
+      return None
+    if len(bundles)>1:
+      logging.error('Non-unique bundle id detected: "'+str(bid)+'"')
+      # This should never happen, so if it does, something has gone horribly
+      # wrong and we probably can't trust any of the results. 
+      return None
+    return bundles[0]
+
 
 def search_experiments(pattern):
   assert config.storage in storage_mechanisms, 'Could not find a data storage adapter name "'+config.storage+'"'
   datastore = storage_mechanisms[config.storage]()
   xids = datastore.find_experiments(pattern)
   return [Query_Result(datastore,xid) for xid in xids]
+
+def exact_experiment(xid):
+  assert config.storage in storage_mechanisms, 'Could not find a data storage adapter name "'+config.storage+'"'
+  datastore = storage_mechanisms[config.storage]()
+  xids = list(datastore.find_experiments({'metadata':{'id':str(xid)}}))
+  if len(xids)==0:
+    logging.debug('No experiment id matched "'+str(xid)+'"')
+    return None
+  if len(xids)>1:
+    logging.error('Non-unique experiment id detected: "'+str(xid)+'"')
+    # This should never happen, so if it does, something has gone horribly
+    # wrong and we probably can't trust any of the results. 
+    return None
+  return Query_Result(datastore,xids[0])
