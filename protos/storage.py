e5b161439cae7c0a0ec55f9152710a89958077fb
Bob Adolf <rdadolf@gmail.com>
2015-08-25 13:32:17 -0400

    Allow protos to function without all storage_adapters.
    Storage adapters are the most common missing support package for protos.
    Moreover, sometimes we don't even need all of them. Instead of forcing the
    user to install unnecessary packages, we allow protos to start with missing
    storage adapters disabled. Obviously, if the user tries to use them, protos
    will complain about a missing adapter, but otherwise it should function fine.
    
    The current behavior is to warn on a failed storage adapter.

--- protos/storage.py
+++ protos/storage.py
@@ -1,11 +1,25 @@
-from .storage_adapters.fake import Fake
-from .storage_adapters.mongo import Mongo
-from .storage_adapters.disk import Disk
-from .storage_adapters.postgres import Postgres
+mechanisms = {}
 
-mechanisms = {
-  'fake' : Fake,
-  'mongo' : Mongo,
-  'postgres' : Postgres,
-  'disk' : Disk
-}
+try:
+  from .storage_adapters.fake import Fake
+  mechanisms['fake'] = Fake
+except e:
+  logging.warn('Failed to import storage adapter "Fake": '+str(e))
+
+try:
+  from .storage_adapters.mongo import Mongo
+  mechanisms['mongo'] = Mongo
+except e:
+  logging.warn('Failed to import storage adapter "Mongo": '+str(e))
+
+try:
+  from .storage_adapters.disk import Disk
+  mechanisms['disk'] = Disk
+except e:
+  logging.warn('Failed to import storage adapter "Disk": '+str(e))
+
+try:
+  from .storage_adapters.postgres import Postgres
+  mechanisms['postgres'] = Postgres
+except e:
+  logging.warn('Failed to import storage adapter "Postgres": '+str(e))
